faf7228b39367281838088fa130a8b82e2b0a647
SB <sarahbrown.slb@gmail.com>
2024-11-11 21:53:19 +1030

    config updates to support component library package

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Component from "./Component";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
             Get started by editing <code>src/app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
+          <Component />
         </ol>
 
         <div className={styles.ctas}>
