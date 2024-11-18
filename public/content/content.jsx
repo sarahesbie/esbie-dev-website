e689a469593f7180be001b54a4035d4dd8aa9e27
SB <sarahbrown.slb@gmail.com>
2024-11-18 16:21:36 +1030

    added images and content

---
+++ public/content/content.jsx
@@ -0,0 +1,149 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLinkedin,
+  faReact,
+  faNodeJs,
+  faAws,
+  faJs,
+  faPython,
+  faGit,
+  faGithub,
+  faSass,
+  faFigma,
+  faNpm,
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  faN,
+  faSquareCaretUp,
+  faT,
+  faLaptopCode,
+  faDatabase,
+  faServer,
+  faBrain,
+  faTicket,
+} from "@fortawesome/free-solid-svg-icons";
+
+export const techIcons = [
+  {
+    icon: <FontAwesomeIcon icon={faReact} />,
+    caption: "React JS",
+  },
+  {
+    icon: <FontAwesomeIcon icon={faNodeJs} />,
+    caption: "Node.js",
+  },
+  {
+    icon: <FontAwesomeIcon icon={faAws} />,
+    caption: "AWS",
+  },
+  {
+    icon: <FontAwesomeIcon icon={faJs} />,
+    caption: "JavaScript",
+  },
+  {
+    icon: <FontAwesomeIcon icon={faPython} />,
+    caption: "Python",
+  },
+  {
+    icon: <FontAwesomeIcon icon={faN} />,
+    caption: "Next.js",
+  },
+  {
+    icon: <FontAwesomeIcon icon={faSquareCaretUp} />,
+    caption: "Vercel",
+  },
+  {
+    icon: <FontAwesomeIcon icon={faGit} />,
+    caption: "git",
+  },
+  {
+    icon: <FontAwesomeIcon icon={faGithub} />,
+    caption: "GitHub",
+  },
+  {
+    icon: <FontAwesomeIcon icon={faT} />,
+    caption: "TypeScript",
+  },
+  {
+    icon: <FontAwesomeIcon icon={faSass} />,
+    caption: "Sass",
+  },
+  {
+    icon: <FontAwesomeIcon icon={faLaptopCode} />,
+    caption: "VS Code",
+  },
+  {
+    icon: <FontAwesomeIcon icon={faFigma} />,
+    caption: "Figma",
+  },
+  {
+    icon: <FontAwesomeIcon icon={faDatabase} />,
+    caption: "NoSQL",
+  },
+  {
+    icon: <FontAwesomeIcon icon={faServer} />,
+    caption: "Serverless",
+  },
+  {
+    icon: <FontAwesomeIcon icon={faNpm} />,
+    caption: "NPM",
+  },
+  {
+    icon: <FontAwesomeIcon icon={faBrain} />,
+    caption: "OpenAI",
+  },
+  {
+    icon: <FontAwesomeIcon icon={faTicket} />,
+    caption: "JIRA",
+  },
+];
+
+export const linkIcons = [
+  {
+    icon: <FontAwesomeIcon icon={faLinkedin} />,
+    caption: "LinkedIn",
+  },
+  {
+    icon: <FontAwesomeIcon icon={faGithub} />,
+    caption: "GitHub",
+  },
+];
+
+export const cardData = [
+  {
+    title: "Project 1",
+    description: "A description of Project 1.",
+    link: "https://example.com/project1",
+    backgroundImage: "https://picsum.photos/300/200?random=1",
+  },
+  {
+    title: "Project 2",
+    description: "A description of Project 2.",
+    link: "https://example.com/project2",
+    backgroundImage: "https://picsum.photos/300/200?random=2",
+  },
+  {
+    title: "Project 3",
+    description: "A description of Project 3.",
+    link: "https://example.com/project3",
+    backgroundImage: "https://picsum.photos/300/200?random=3",
+  },
+  {
+    title: "Project 3",
+    description: "A description of Project 3.",
+    link: "https://example.com/project3",
+    backgroundImage: "https://picsum.photos/300/200?random=3",
+  },
+  {
+    title: "Project 3",
+    description: "A description of Project 3.",
+    link: "https://example.com/project3",
+    backgroundImage: "https://picsum.photos/300/200?random=3",
+  },
+  {
+    title: "Project 3",
+    description: "A description of Project 3.",
+    link: "https://example.com/project3",
+    backgroundImage: "https://picsum.photos/300/200?random=3",
+  },
+];
