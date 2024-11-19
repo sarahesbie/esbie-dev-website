0c9c103c18ae573a037c29a487bf7166815aedc6
SB <sarahbrown.slb@gmail.com>
2024-11-19 12:15:36 +1030

    [CONFIG] set up Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.scss";
 import { azeretMono, figtree } from "../libs/fonts";
 import "../libs/fontawesome";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Sarah Brown | Full Stack Web Developer",
