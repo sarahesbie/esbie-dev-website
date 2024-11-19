050f396c0fc023d616778a894bbf96b5b783f2e8
SB <sarahbrown.slb@gmail.com>
2024-11-19 15:08:41 +1030

    added analytics component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${figtree.variable} ${azeretMono.variable}`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
